2b7f5a08fa88304ccce6c43aad4111c291dc0d26
Apoorv Verma <rishuverma870735@gmail.com>
2025-07-02 18:22:07 +0530

    Bootstrap Practice

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      {/* <div className='container-fluid '>
+    <div className='row row-cols-3'>
+      <div className="col col">Col 1</div>
+      <div className="col co">Col 2</div>
+      <div className="col col">Col 3</div>
+      <div className="col col">Col 4</div>
+      <div className="col col">Col 5</div>
+    </div>
+    <div className='row'>Row 2</div>
+    <div className='row'>Row 3</div>
+    <div className='row'>Row 4</div>  
+      </div> */}
+      
+    </div>
+  )
+}
+
+export default App
